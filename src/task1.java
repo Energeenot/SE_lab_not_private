457d7813cfe3663a5f586ee33b4c64868b7ef397
Energeenot <johndoe@abramov@pandos@yandex.ru>
2023-02-21 11:09:20 +0300

    add math action

--- src/task1.java
+++ src/task1.java
@@ -2,8 +2,15 @@ import java.util.Scanner;
 
 public class task1 {
     public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 //        String name = scanner.nextLine();
         System.out.println("Hello, " + args[0]);
+
+        System.out.println("x  =");
+        double x = scanner.nextDouble();
+        System.out.println("y = ");
+        double y =  scanner.nextDouble();
+        double z = ((3*Math.pow(Math.cos(x - Math.PI/6),2.0))/0.5 + Math.sin(y)*Math.sin(y));
+        System.out.println("z = " + z);
     }
 }
